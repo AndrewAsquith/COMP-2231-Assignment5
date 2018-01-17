c13eec723dd996002a1d44d9a4d3e29ad3447cce
Andrew Asquith <contact@andrewasquith.com>
2018-01-16 21:51:47 -0500

    Removed dual version of heap sorts, left traditional ascending, added extra heap sort tests

--- src/com/andrewasquith/comp2231/assignment5/question1/HeapSort.java
+++ src/com/andrewasquith/comp2231/assignment5/question1/HeapSort.java
@@ -5,9 +5,9 @@ public class HeapSort<T> {
 	/**
 	 * Performs a Heap Sort returning the array in ascending order
 	 * 
-	 * @param data
+	 * @param data the array to be sorted
 	 */
-	public void AscendingHeapSort(T[] data) {
+	public void HeapSort(T[] data) {
 
 		// start at first non leaf node
 		// would be length - 1 / 2
@@ -18,7 +18,7 @@ public class HeapSort<T> {
 		// result and we're going to eventually treat the array as two parts
 		// unsorted heap - | sorted
 		// [new root][][][][] | [][largest]
-		// otherwise we'd have to shift the sorted on every removal
+		// otherwise we'd have to shift the sorted portion on every iteration
 		// or alternatively we could have reversed the resulting array
 		// but both methods would be less efficient
 		for (int i = start; i >= 0; i--) {
@@ -41,10 +41,10 @@ public class HeapSort<T> {
 			// store the new root
 			data[0] = data[last];
 
-			// store the next max at the end
+			// store the next max in the next sorted index
 			data[last] = temp;
 
-			// reheapify what remains
+			// re-heapify what remains
 			maxHeapify(data, 0, last - 1);
 
 		}
@@ -52,11 +52,11 @@ public class HeapSort<T> {
 	}
 
 	/**
-	 * Adjust the subtree starting at root and ending to end to be a valid heap
+	 * Adjust the heap starting at root and ending at end to be a valid max heap
 	 * 
-	 * @param data
-	 * @param root
-	 * @param end
+	 * @param data the array containing the heap
+	 * @param root the index of the root of the heap to adjust
+	 * @param end the index of the last node in the heap to adjust
 	 */
 	private void maxHeapify(T[] data, int root, int end) {
 
@@ -87,84 +87,7 @@ public class HeapSort<T> {
 			// then need to heapify the subtree after the swap
 			maxHeapify(data, largest, end);
 		}
-		
 
 	}
-
-	private void minHeapify(T[] data, int root, int end) {
-
-		int i = root;
-		int left = 2 * i + 1;
-		int right = 2 * (i + 1);
-
-		// assume parent smallest to start
-		int smallest = i;
-
-		// if left child is within bounds and smaller than root make it smaller
-		if (left <= end && (((Comparable) data[smallest]).compareTo(data[left]) > 0)) {
-			smallest = left;
-		}
-
-		// if right child is within bounds and smaller than left and root make
-		// it smallest
-		if (right <= end && (((Comparable) data[smallest]).compareTo(data[right]) > 0)) {
-			smallest = right;
-		}
-
-		// if the parent is not the largest, swap
-		if (smallest != i) {
-			T temp = data[smallest];
-			data[smallest] = data[i];
-			data[i] = temp;
-
-			// then need to heapify the subtree after the swap
-			minHeapify(data, smallest, end);
-		}
-
-	}
-
-	public void DescendingHeapSort(T[] data) {
-		// start at first non leaf node
-				// would be length - 1 / 2
-				int start = (data.length - 1) / 2;
-
-				// build the heap from the existing elements
-				// need to build a max heap since we want a ascending sort order as
-				// result and we're going to eventually treat the array as two parts
-				// unsorted heap - | sorted
-				// [new root][][][][] | [][largest]
-				// otherwise we'd have to shift the sorted on every removal
-				// or alternatively we could have reversed the resulting array
-				// but both methods would be less efficient
-				for (int i = start; i >= 0; i--) {
-					minHeapify(data, i, data.length - 1);
-				}
-
-				// loop over array again
-				// remove max by moving it to the sorted portion
-				// then re-heapify what remains in the heap
-				// equivalent to the traditional "remove" operation
-
-				for (int i = 0; i < data.length; i++) {
-
-					// shrink the heap portion of the array
-					int last = data.length - (i + 1);
-
-					// remove max and store temporarily
-					T temp = data[0];
-
-					// store the new root
-					data[0] = data[last];
-
-					// store the next max at the end
-					data[last] = temp;
-
-					// re-heapify what remains
-					minHeapify(data, 0, last - 1);
-
-				}
-	}
-
-
 
 }
--- src/com/andrewasquith/comp2231/assignment5/question1/HeapSortTests.java
+++ src/com/andrewasquith/comp2231/assignment5/question1/HeapSortTests.java
@@ -7,25 +7,35 @@ import org.junit.Test;
 public class HeapSortTests {
 
 	@Test
-	public final void testAscHeapSortSixElements() {
+	public final void testHeapSortSixElements() {
 		
 		Integer[] starting = { 18, 21, 11, 5, 17, 9};
 		Integer[] expected = { 5, 9, 11, 17, 18, 21};
 		
 		HeapSort<Integer> heaper = new HeapSort<Integer>();
-		heaper.AscendingHeapSort(starting);
+		heaper.HeapSort(starting);
 		
 		assertArrayEquals(expected, starting);
 	}
 	
 	@Test
-	public final void testDescHeapSortSixElements() {
-		
-		Integer[] starting = { 18, 21, 11, 5, 17, 9};
-		Integer[] expected = { 21, 18, 17, 11, 9, 5};
+	public final void testHeapSortNineElements() {
+		Integer[] starting = { 13, 11, 15, 16, 7, 3, 21, 5, 18 };
+		Integer[] expected = { 3, 5, 7, 11, 13, 15, 16, 18, 21 };
 		
 		HeapSort<Integer> heaper = new HeapSort<Integer>();
-		heaper.DescendingHeapSort(starting);
+		heaper.HeapSort(starting);
+		
+		assertArrayEquals(expected, starting);
+	}
+	
+	@Test
+	public final void testHeapSortNineElementsWithOneDuplicate() {
+		Integer[] starting = { 13, 11, 15, 16, 7, 3, 21, 5, 16 };
+		Integer[] expected = { 3, 5, 7, 11, 13, 15, 16, 16, 21 };
+		
+		HeapSort<Integer> heaper = new HeapSort<Integer>();
+		heaper.HeapSort(starting);
 		
 		assertArrayEquals(expected, starting);
 	}
